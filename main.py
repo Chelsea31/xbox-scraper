a5f0fb1de6844c93ade1dc7dc3ce305b1ac0efe6
Akanksha Grover <akanksha@everwell.org>
2021-10-09 19:32:54 +0530

    check xbox availability on Vijay Sales website

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from bs4 import BeautifulSoup
 amazon_urls = ["https://www.amazon.in/Xbox-Series-X/dp/B08J7QX1N1"]
 flipkart_urls = ["https://www.flipkart.com/microsoft-xbox-series-x-1024-gb/p/itm63ff9bd504f27"]
 reliance_url = ["https://www.reliancedigital.in/xbox-series-x-console-with-wireless-controller-1-tb/p/491934660"]
-croma_urls = ["https://www.croma.com/microsoft-xbox-one-x-1-tb-console-+-shadow-of-the-tomb-raider-bundle-cyv-00118-white-/p/215372"]
+vs_urls = ["https://www.vijaysales.com/xbox-series-x-gaming-console-1-tb/16215"]
 user_agent_list = []
 
 
@@ -58,11 +58,11 @@ def check_reliance(url):
         return False
 
 
-def check_croma(url):
+def check_vijaySales(url):
     soup = getSoup(url)
-    button_list = list(soup.find_all("button", class_="pdp-add-to-cart"))
+    button_list = list(soup.find_all("div", attrs={"class":"btnsCartBuy", "style":"display:block"}))
     if button_list:
-        print("Found stock on Croma : " + url)
+        print("Found stock on Vijay Sales : " + url)
         return True
     else:
         return False
@@ -73,5 +73,5 @@ if __name__ == '__main__':
     result_rd = [check_reliance(url) for url in reliance_url]
     result_amazon = [check_amazon(url) for url in amazon_urls]
     result_flipkart = [check_flipkart(url) for url in flipkart_urls]
-    result_croma = [check_croma(url) for url in croma_urls]
+    result_vijaySales = [check_vijaySales(url) for url in vs_urls]
     print("The end..")
